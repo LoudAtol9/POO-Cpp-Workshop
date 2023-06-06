83d62165144607c54eec24661efe666879bb1c03
André Pupin <70980214+LoudAtol9@users.noreply.github.com>
2023-06-06 15:11:32 -0300

    Create Endereco.hpp

---
+++ Endereco.hpp
@@ -0,0 +1,39 @@
+#ifndef ENDERECO_H
+#define ENDERECO_H
+
+#include <iostream>
+
+class Endereco
+{
+private:
+	std::string rua;
+	std::string bairo;
+	int cep;
+	std::string pais;
+	std::string estado;
+	std::string cidade;
+	
+public:
+	Endereco();
+	~Endereco();
+	
+	std::string getRua();
+	void setRua(std::string new_rua);
+	
+	std::string getBairo();
+	void setBairo(std::string new_Bairo);
+	
+	int getCep();
+	void setCep(int new_cep);
+	
+	std::string getPais();
+	void setPais(std::string new_pais);
+	
+	std::string getEstado();
+	void setEstado(std::string new_estado);
+	
+	std::string getRua();
+	void setRua(std::string new_rua);
+}
+
+#endif // ENDERECO_H
